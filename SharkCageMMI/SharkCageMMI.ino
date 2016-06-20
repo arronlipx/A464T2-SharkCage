15f792847c5e7af38612738a04fceecfa8e2ada2
arronlipx <arron2000@hotmail.com>
2016-06-21 01:22:22 +0800

    Used Button Library
    Working Now

--- SharkCageMMI/SharkCageMMI.ino
+++ SharkCageMMI/SharkCageMMI.ino
@@ -1,3 +1,5 @@
+#include <Button.h>
+
 const byte numChars = 32;
 char receivedChars[numChars];  // an array to store the received data
 
@@ -14,16 +16,18 @@ const char FAST2 = 4;
 char state=0;
 
 boolean newData = false;
-const int fastSwitch = 12;  //connect pin 12 to fast switch
-const int selSwitch = 11;  	//connect pin 11 to select switch
+
+Button fastSwitch (12,PULLUP);  //connect pin 12 to fast switch
+Button selSwitch (11,PULLUP);  	//connect pin 11 to select switch
 const int fastLed = 10;		//connect pin 10 to fastLed Relay
 const int selLed = 9;		//connect pin 9 to selectLed Relay
 const int joystickPin = A0;	//connect pin A0 to joystick 
 
 //connect to other box
 
-const int fastSwitch2 = 8;  //connect pin 8 to second fast switch
-const int selSwitch2 = 7;   //connect pin 7 to second select switch
+Button fastSwitch2 (8,PULLUP);  //connect pin 8 to second fast switch
+Button selSwitch2 (7,PULLUP);   //connect pin 7 to second select switch
+
 const int fastLed2 = 6;   //connect pin 6 to second fastLed Relay
 const int selLed2 = 5;   //connect pin 5 to second selectLed Relay
 const int joystickPin2 = A1; //connect pin A1 to second joystick 
@@ -52,28 +56,52 @@ boolean selSwitchTemp2=false;
 void setup() {
   // put your setup code here, to run once
   Serial.begin(9600);
-  pinMode(fastSwitch, INPUT_PULLUP);
-  pinMode(selSwitch, INPUT_PULLUP);
-  pinMode(fastLed, OUTPUT);0
+  pinMode(fastLed, OUTPUT);
   pinMode(selLed, OUTPUT);
-  pinMode(joystickPin, INPUT);//analogue in
+  pinMode(joystickPin, INPUT);//analog in
 
-  pinMode(fastSwitch2, INPUT_PULLUP);
-  pinMode(selSwitch2, INPUT_PULLUP);
   pinMode(fastLed2, OUTPUT);
   pinMode(selLed2, OUTPUT);
-  pinMode(joystickPin2, INPUT);//analogue in
+  pinMode(joystickPin2, INPUT);//analog in
 
-  pinMode(roboteqCmd, OUTPUT);//analoue out
+  pinMode(roboteqCmd, OUTPUT);//analog out
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  fastSwitchTemp = readButton(fastSwitch);
-  fastSwitchTemp2 = readButton(fastSwitch2);
-  selSwitchTemp = readButton(selSwitch);
-  selSwitchTemp2 = readButton(selSwitch2);
+//
+//  if(fastSwitch.uniquePress())
+//  {
+//    fastSwitchTemp=!fastSwitchTemp;
+//
+//  }
+//      Serial.print("fastSwitch:");
+//    Serial.println(fastSwitchTemp);
+//
+//  if(fastSwitch2.uniquePress())
+//  {
+//    fastSwitchTemp2=!fastSwitchTemp2;
+//    
+//  }
+//  Serial.print("fastSwitch2:");
+//    Serial.println(fastSwitchTemp2);
+//  if(selSwitch.uniquePress())
+//  {
+//    selSwitchTemp=!selSwitchTemp;
+//   
+//  }
+//   Serial.print("selSwitch:");
+//    Serial.println(selSwitchTemp);
+//
+//  if(selSwitch2.uniquePress())
+//  {
+//    selSwitchTemp2=!selSwitchTemp2;
+//
+//  }
+//      Serial.print("selSwitch2:");
+//    Serial.println(selSwitchTemp2);
+
 
   if (state==NONE)
   {
@@ -82,9 +110,16 @@ void loop() {
     digitalWrite(selLed2,LOW);
     digitalWrite(fastLed2,LOW);
 
-  	if(readButton(selSwitch))
+    analogWrite(roboteqCmd, 128);
+
+  	if(selSwitch.uniquePress())
   		state=SELECTED;
-    else if (readButton()) 
+    else if (selSwitch2.uniquePress())
+      state=SELECTED2; 
+
+    Serial.println("NONE");
+
+
   }
 
   else if (state==SELECTED)
@@ -94,7 +129,13 @@ void loop() {
     digitalWrite(selLed2,LOW);
     digitalWrite(fastLed2,LOW);
 
-  	analogWrite(roboteqCmd, map(analogRead(joystickPin), 0, 1023, 64, 192 ));
+  	analogWrite(roboteqCmd, map(analogRead(joystickPin), 0, 1023, 64, 192 ));//input is 10 bit, output is 8 bit
+
+    if(fastSwitch.uniquePress())
+      state=FAST;
+    else if (selSwitch2.uniquePress())
+      state=SELECTED2;
+    Serial.println("SELECTED");
   }
   else if(state==FAST)
   {
@@ -104,6 +145,12 @@ void loop() {
     digitalWrite(fastLed2,LOW);
 
     analogWrite(roboteqCmd, map(analogRead(joystickPin), 0, 1023, 0, 255 ));
+
+    if(fastSwitch.uniquePress())
+      state=SELECTED;
+    else if (selSwitch2.uniquePress())
+      state=SELECTED2;
+    Serial.println("FAST");
   }
   else if(state==SELECTED2)
   {
@@ -112,7 +159,13 @@ void loop() {
     digitalWrite(selLed2,HIGH);
     digitalWrite(fastLed2,LOW);
 
-    analogWrite(roboteqCmd, map(analogRead(joystickPin2), 0, 1023, 64, 192 ));
+    analogWrite(roboteqCmd, map(analogRead(joystickPin2), 0, 1023, 192, 64 ));//reversed to account for side
+
+    if(fastSwitch2.uniquePress())
+      state=FAST2;
+    else if (selSwitch.uniquePress())
+      state=SELECTED;
+    Serial.println("SELECTED2");
   }
   else if(state==FAST2)
   {
@@ -121,121 +174,21 @@ void loop() {
     digitalWrite(selLed2,HIGH);
     digitalWrite(fastLed2,HIGH);
 
-    analogWrite(roboteqCmd, map(analogRead(joystickPin2), 0, 1023, 0, 255 ));
+    analogWrite(roboteqCmd, map(analogRead(joystickPin2), 0, 1023, 255, 0 ));
+
+    if(fastSwitch2.uniquePress())
+      state=SELECTED2;
+    else if (selSwitch.uniquePress())
+      state=SELECTED;
+    Serial.println("FAST2");
   }
+  delay(1000);
 
 }
 
 //returns true if a new button press is detected
 //holding the button does not toggle on/off
-boolean readButton(int buttonPin)
-{
 
-	boolean currentState=digitalRead(buttonPin);
-	if (currentState==HIGH)
-	{
-    if (buttonPin==fastSwitch)
-    {
-      if(fastSwitchFlag==true)
-      {
-        return false;
-      }
-      else
-      {
-        fastSwitchFlag=true;
-        return true;
-      }
-    }
-    if (buttonPin==selSwitch)
-    {
-      if(selSwitchFlag==true)
-      {
-        return false;
-      }
-      else
-      {
-        selSwitchFlag=true;
-        return true;
-      }
-    }
-    if (buttonPin==fastSwitch2)
-    {
-      if(fastSwitchFlag2==true)
-      {
-        return false;
-      }
-      else
-      {
-        fastSwitchFlag2=true;
-        return true;
-      }
-    }
-    if (buttonPin==selSwitch2)
-    {
-      if(selSwitchFlag2==true)
-      {
-        return false;
-      }
-      else
-      {
-        selSwitchFlag2=true;
-        return true;
-      }
-    }
-	}
-	else
-	{
-		if (buttonPin==fastSwitch)
-		{
-			fastSwitchFlag=false;
-			return false;
-		}
-		if (buttonPin==selSwitch)
-		{
-			selSwitchFlag=false;
-			return false;
-		}
-    if (buttonPin==fastSwitch2)
-    {
-      fastSwitchFlag2=false;
-      return false;
-    }
-    if (buttonPin==selSwitch2)
-    {
-      selSwitchFlag2=false;
-      return false;
-    }
-	}
-
-	// boolean currentState = LOW;//storage for current measured button state
-	// boolean lastState = LOW;//storage for last measured button state
-	// boolean debouncedState = LOW;//debounced button state
-
-	// int debounceInterval = 20;//wait 20 ms for button pin to settle
-	// unsigned long timeOfLastButtonEvent = 0;//store the last time the button state changed
-
-	// currentState = digitalRead(buttonPin);
- //  	unsigned long currentTime = millis();
-  
- //  if (currentState != lastState){
- //    timeOfLastButtonEvent = currentTime;
- //  }
-  
- //  if (currentTime - timeOfLastButtonEvent > debounceInterval){//if enough time has passed
- //    if (currentState != debouncedState){//if the current state is still different than our last stored debounced state
- //      debouncedState = currentState;//update the debounced state
-      
- //      //trigger an event
- //      if (debouncedState == HIGH){
- //        Serial.println("pressed");
- //      } else {
- //        Serial.println("released");
- //      }
- //    }
- //  }
-  
- //  lastState = currentState;
-}
 void recvWithEndMarker() {
   static byte ndx = 0;
   char endMarker = '\n';
